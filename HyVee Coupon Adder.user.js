7be06ee8d3ce768276ca7df59ca82ae848c058fb
Joseph Watts <josephcw@live.com>
2018-08-15 08:54:19 -0700

    Fix problem with script refreshing

--- HyVee Coupon Adder.user.js
+++ HyVee Coupon Adder.user.js
@@ -6,6 +6,7 @@
 // ==/UserScript==
 
 var refreshDelayInSeconds = 10;
+var initialDelayInSeconds = 8;
 var addButtonName = "add-to-cart";
 
 // Function to click on an element.
@@ -34,9 +35,17 @@ setTimeout(function() {
     coupons = document.getElementsByClassName(addButtonName);
     for (i = 0; i < coupons.length; i++) {
       click(coupons[i])
-    } 
+    }
+    
+    // Refresh the page
+    setTimeout(
+    function() {
+      console.log("Refreshing the page");
+    	location.reload(); 
+  	}, refreshDelayInSeconds * 1000);
+    
   } catch (e) {
   	console.log("Encountered an exception while parsing Hy-Vee deals!");
   }
-}, refreshDelayInSeconds * 1000);
+}, initialDelayInSeconds * 1000);
 
